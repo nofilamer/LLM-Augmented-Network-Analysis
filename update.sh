a18adf201add2b25c2d5b6f3a72bd438f4f92d00
Nofil Amer <nofilamer@gmail.com>
2025-02-21 22:48:11 -0800

    Resolved merge conflicts

--- update.sh
+++ update.sh
@@ -2,3 +2,9 @@ git add .
 git commit -m "Update"
 git push origin main
 
+#What to do when local branch is out of sync with the remote branch.
+# This usually happens when the remote repository has new commits that you don’t have locally.
+#git fetch origin
+#git status
+#git pull origin main --rebase
+
